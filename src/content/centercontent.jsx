0151a8d668439e9aafdb40c438faca00cc9dc3a7
Ulaganathan <Kalaiselvan.Ulaganathan@mastercard.com>
2018-02-19 03:09:01 -0600

    Event handling logic added

--- src/content/centercontent.jsx
+++ src/content/centercontent.jsx
@@ -4,10 +4,22 @@ import ReactDOM from 'react-dom';
 import {Form, FormGroup, FormControl, ControlLabel, Col, Button} from 'react-bootstrap';
 
 export default class CenterContent extends Component {
+  
+  constructor(){
+    super();
+    this.state={"buttonName": "Submit"};
+    this.handleClick=this.handleClick.bind(this);
+  }
+
+  handleClick(e){
+    console.log("Clicked !!");
+    e.preventDefault();
+    this.setState({ buttonName: "Clicked"});
+  }
 
   render() {
     return (
-      <Form horizontal>
+      <Form horizontal onSubmit={this.handleClick}>
         <FormGroup controlId="taskName">
           <Col componentClass={ControlLabel} xs={4} md={2}>
             Task Name:
@@ -34,7 +46,7 @@ export default class CenterContent extends Component {
           <Col componentClass={ControlLabel} xs={4} md={2}>
           </Col>
           <Col xs={8} md={4}>
-          <Button type="submit">Submit</Button>
+          <Button type="submit" >{this.state.buttonName}</Button>
           </Col>
         </FormGroup>
       </Form>
